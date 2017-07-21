e9e23f9cd5a66597373a5d7e1e9a40af3cf9f2b7
Jalal Uddin <jalal.exe@gmail.com>
2017-07-21 18:17:37 +0600

    Created new MVC and Test project

---
+++ SotreSite/SotreSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SotreSite.Startup))]
+namespace SotreSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
