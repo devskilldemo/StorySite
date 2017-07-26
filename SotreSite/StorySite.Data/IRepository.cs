0e6620ee2630dbcac4d4e6d51c80e7f8d4c1514c
Jalal Uddin <jalal.exe@gmail.com>
2017-07-26 20:45:33 +0600

    generic repository added

---
+++ SotreSite/StorySite.Data/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace StorySite.Data
+{
+    public interface IRepository<TEntity> where TEntity : Entity
+    {
+        void Delete(object id);
+        void Delete(TEntity entityToDelete);
+        IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderby = null);
+        IEnumerable<TEntity> Get(out int total, out int totalDisplay, Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderby = null, int pageIndex = 1, int pageSize = 10);
+        TEntity GetByID(object id);
+        int GetCount(Expression<Func<TEntity, bool>> filter = null);
+        void Insert(TEntity entity);
+        void Update(TEntity entityToUpdate);
+    }
+}
---
+++ SotreSite/StorySite.Data/Repository.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorySite.Data
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity
+    {
+        protected DbContext context;
+        protected DbSet<TEntity> dbSet;
+
+        public Repository(DbContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<TEntity>();
+        }
+
+        public virtual void Insert(TEntity entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public virtual void Delete(object id)
+        {
+            Delete(GetByID(id));
+        }
+
+        public virtual void Delete(TEntity entityToDelete)
+        {
+            if(context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                dbSet.Attach(entityToDelete);
+            }
+            dbSet.Remove(entityToDelete);
+        }
+
+        public virtual void Update(TEntity entityToUpdate)
+        {
+            dbSet.Attach(entityToUpdate);
+            context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+
+        public virtual TEntity GetByID(object id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public virtual IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null, 
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderby = null)
+        {
+            IQueryable<TEntity> query = dbSet;
+
+            if(filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if(orderby != null)
+            {
+                return orderby(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+        }
+
+        public virtual int GetCount(Expression<Func<TEntity, bool>> filter = null)
+        {
+            IQueryable<TEntity> query = dbSet;
+            int count = query.Count();
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+                count = query.Count();
+            }
+            return count;
+        }
+
+        public virtual IEnumerable<TEntity> Get(out int total, out int totalDisplay,
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderby = null,
+            int pageIndex = 1, int pageSize = 10)
+        {
+            IQueryable<TEntity> query = dbSet;
+            total = query.Count();
+            totalDisplay = query.Count();
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+                totalDisplay = query.Count();
+            }
+
+            if (orderby != null)
+            {
+                return orderby(query).Skip((pageIndex  - 1) * pageSize).Take(pageSize).ToList();
+            }
+            else
+            {
+                return query.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
+            }
+        }
+    }
+}
