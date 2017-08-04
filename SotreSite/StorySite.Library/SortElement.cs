39a47459f943c4a88647588f7502d8fca3840af7
Jalal Uddin <jalal.exe@gmail.com>
2017-08-04 20:17:02 +0600

    added model class for datatables

---
+++ SotreSite/StorySite.Library/SortElement.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlClient;
+
+namespace StorySite
+{
+    public class SortElement
+    {
+        private string comlumnName;
+        private SortOrder sortOrder;
+
+        public SortElement(string comlumnName, SortOrder sortOrder)
+        {
+            this.comlumnName = comlumnName;
+            this.sortOrder = sortOrder;
+        }
+    }
+}
